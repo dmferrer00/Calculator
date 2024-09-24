58988d9f7accd989bdadd9382b36c2213518551f
Daniel Ferrer <dferrer@uncc.edu>
2024-09-24 19:31:48 -0400

    feat: completed add functions for goodCalc and badCalc

--- badCalc.py
+++ badCalc.py
@@ -1,2 +1,4 @@
+## this is a function (EXAMPLE OF BAD DOCUMENTING
+
 def a(num1, num2):
     return num1 + num2
---
+++ goodCalc.py
@@ -0,0 +1,3 @@
+## this is a function that adds two numbers
+def add(num1, num2):
+    return num1 + num2
